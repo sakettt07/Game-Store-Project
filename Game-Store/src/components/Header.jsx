b5e0d0b304e00971fc5a321b0a64b727214e2aaa
saket <saket@saketgupta.com>
2023-12-11 00:31:28 +0530

    New Project

---
+++ Game-Store/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './../assets/images/store-logo.png';
+
+const Header = () => {
+  return (
+    <div>
+      <img src={logo} alt="" />
+    </div>
+  )
+}
+
+export default Header
