fdc67575651a3cd2ea8ac3ba82db27083173a906
saket <saket@saketgupta.com>
2023-12-15 08:36:56 +0530

    Header-work

--- Game-Store/src/components/Header.jsx
+++ Game-Store/src/components/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import logo from './../assets/images/store-logo.png';
+import { IoSearch } from 'react-icons/io5';
+import { FaMoon } from "react-icons/fa";
+import { FaSun } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div>
-      <img src={logo} alt="" />
+    <div className='flex items-center mx-5'>
+      <img src={logo} width={240} height={60} className='p-5' />
+      <div className='flex bg-slate-50 p-4 mt-2 items-center w-[70%] rounded-full'>
+        <IoSearch />
+        <input type="text" className='outline-none px-4' />
+      </div>
+      <div className='px-7 pt-5 w-[50px] h-[54px] '>
+        <FaMoon  />
+        <FaSun />
+      </div>
     </div>
   )
 }
