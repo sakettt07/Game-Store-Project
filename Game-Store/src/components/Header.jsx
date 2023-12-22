190878445502ee32d9ab901976825c26bc5db058
saket <saket@saketgupta.com>
2023-12-22 18:41:38 +0530

    Navbar-completed

--- Game-Store/src/components/Header.jsx
+++ Game-Store/src/components/Header.jsx
@@ -1,23 +1,37 @@
-import React from 'react';
-import logo from './../assets/images/store-logo.png';
-import { IoSearch } from 'react-icons/io5';
+import React, { useState } from "react";
+import logo from "./../assets/images/store-logo.png";
+import { IoSearch } from "react-icons/io5";
 import { FaMoon } from "react-icons/fa";
 import { FaSun } from "react-icons/fa";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
   return (
-    <div className='flex items-center mx-5'>
-      <img src={logo} width={240} height={60} className='p-5' />
-      <div className='flex bg-slate-50 p-4 mt-2 items-center w-[70%] rounded-full'>
+    <div className="flex items-center mx-5">
+      <img src={logo} width={240} height={60} className="p-5" />
+      <div className="flex bg-slate-50 p-4 mt-2 items-center w-[70%] rounded-full">
         <IoSearch />
-        <input type="text" className='outline-none px-4' />
+        <input
+          placeholder="Search Games"
+          type="text"
+          className="outline-none px-3 bg-slate-50"
+        />
       </div>
-      <div className='px-7 pt-5 w-[50px] h-[54px] '>
-        <FaMoon  />
-        <FaSun />
+      <div className="px-7 pt-5 w-[50px] h-[54px] ">
+        {toggle ? (
+          <FaMoon
+            className="bg-slate-500 text-[35px] p-1 text-black rounded-full cursor-pointer mt-[-6px]"
+            onClick={() => setToggle(!toggle)}
+          />
+        ) : (
+          <FaSun
+            className="bg-slate-500 text-[40px] te p-1 text-black rounded-full cursor-pointer mt-[-6px]"
+            onClick={() => setToggle(!toggle)}
+          />
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
