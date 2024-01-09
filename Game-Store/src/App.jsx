dcaa64860abc67b58a36591c01088246d348dafb
saket <saket@saketgupta.com>
2024-01-09 23:31:44 +0530

    made the app preloader

--- Game-Store/src/App.jsx
+++ Game-Store/src/App.jsx
@@ -3,22 +3,39 @@ import "./App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import { ThemeContext } from "./Context/ThemeContext";
+import Lottie from "lottie-react";
+import loader from "./assets/images/loader-2.json";
 
 function App() {
   const [theme, setTheme] = useState("dark");
-  useEffect(()=>{
-    setTheme(localStorage.getItem('theme')?localStorage.getItem('theme'):'dark');
-  },[])
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+  useEffect(() => {
+    setTheme(
+      localStorage.getItem("theme") ? localStorage.getItem("theme") : "dark"
+    );
+  }, []);
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
-      <div
-        className={`${theme} ${
-          theme == "dark" ? "bg-black" : "bg-cyan-200"
-        } min-h-[100vh] `}
-      >
-        <Header />
-        <Home />
-      </div>
+      {loading ? (
+        <div className="bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-gray-300 via-slate-600 to-black h-screen w-screen flex items-center justify-center">
+          <Lottie animationData={loader} loop={true} className="h-72 w-72" />
+        </div>
+      ) : (
+        <div
+          className={`${theme} ${
+            theme == "dark" ? "bg-black" : "bg-gradient-to-r from-rose-100 to-teal-100"
+          } min-h-[100vh] `}
+        >
+          <Header />
+          <Home />
+        </div>
+      )}
     </ThemeContext.Provider>
   );
 }
